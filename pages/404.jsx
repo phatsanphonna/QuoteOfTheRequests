e61516beef6d28018db423caae10378a48a312bb
ssuniie <phatsanphon2004@outlook.com>
2021-07-16 13:06:51 +0700

    add 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
+import Header from '../components/Header'
+
 export default function Loading() {
     const mainDivTailwindCSS = (
         `flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200`
@@ -16,9 +18,13 @@ export default function Loading() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
+            <Header />
+
             <main className={mainTagTailwindCSS}>
-                <h1 className='text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold m-11 tracking-widest drop-shadow-2xl'>
-                    . . .</h1>
+                <h1
+                    className='text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold m-11 tracking-widest drop-shadow-2xl'>
+                    404
+                </h1>
             </main>
         </div>
     )
