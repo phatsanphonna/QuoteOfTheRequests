3188440c4f46975d185f5fd1d03f22dbd5bbfec7
ssuniie <phatsanphon2004@outlook.com>
2021-07-18 21:54:30 +0700

    update browse.jsx 2

--- components/LongQuoteInfoCard.jsx
+++ components/LongQuoteInfoCard.jsx
@@ -6,29 +6,22 @@ export default function QuoteInfoCards({ quote, name, quoteId }) {
     quote = quote.slice(0, 41) + '...'
   }
 
-  // className='h-24 w-72 xl:w-3/5 bg-white rounded-lg text-left text-black drop-shadow-2xl'
+  //  className='h-24 w-72 xl:w-3/5 bg-white rounded-lg text-left text-black drop-shadow-2xl'
   return (
-    // <div className='h-auto w-auto xl:w-3/5 bg-white rounded-lg text-left text-black drop-shadow-2xl'>
-    //   <div className='m-5 flex flex-initial items-center'>
-    //     <div className='mr-4 items-center'>
-    //       <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold'>{quoteId}</h1>
-    //     </div>
-    //     <div>
-    //       <h1 className='text-base md:text-lg lg:text-xl xl:text-2xl font-bold hover:underline items-center'>
-    //         <Link href={`/quotes/${quoteId}`}><a>{quote}</a></Link></h1>
-    //       <h3 className='text-sm md:text-base lg:text-lg xl:text-xl'>โดย {name}</h3>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className='bg-white'>
-      <div className=''>
-        <h1 className=''>{quoteId}</h1>
-      </div>
-      <div>
-        <h1 className=''>
-          <Link href={`/quotes/${quoteId}`}><a>{quote}</a></Link></h1>
-        <h3 className=''>โดย {name}</h3>
+    <div className='h-30 lg:h-24 w-4/6 bg-white rounded-lg text-left text-black drop-shadow-2xl'>
+      <div className='items-center'>
+        <div className='m-5 flex flex-initial items-center'>
+          <div className='mr-4 items-center'>
+            <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold'>{quoteId}</h1>
+          </div>
+          <div>
+            <h1 className='text-base md:text-lg lg:text-xl xl:text-2xl font-bold hover:underline items-center'>
+              <Link href={`/quotes/${quoteId}`}><a>{quote}</a></Link></h1>
+            <h3 className='text-sm md:text-base lg:text-lg xl:text-xl'>โดย {name}</h3>
+          </div>
+        </div>
       </div>
     </div>
+
   )
 }
--- pages/browse/index.jsx
+++ pages/browse/index.jsx
@@ -40,49 +40,23 @@ export default function index({ data, getTotalQuotes }) {
     }, [quotes])
 
     return (
-      <div className={mainTailwindCSS}>
-        <Header />
+      <InfiniteScroll
+        dataLength={quotes.length} next={getMoreQuotes} hasMore={hasMore}
+      >
+        <div className={mainTailwindCSS}>
+          <Header />
 
-        <div className='flex flex-col items-center justify-center w-full flex-1 px-0'>
-          <InfiniteScroll
-            dataLength={quotes.length} next={getMoreQuotes} hasMore={hasMore}
-            loader={
-              <h4 className='text-white text-lg m-10 flex flex-initial items-center justify-center drop-shadow-lg'>
-                โหลดเพิ่มเติมอยู่นร้าาา รอแป๊ปนึง</h4>}
-            endMessage={
-              <h3 className='text-white text-lg m-10 flex flex-initial items-center justify-center drop-shadow-lg'>
-                หมดแย้ว ๆ</h3>}
-            className='flex-col'
-          >
             {quotes.map((quote) => {
-              // return (
-              //   <LongQuoteInfoCard
-              //     key={quote.sentenceId} quote={quote.sentence}
-              //     name={quote.name} quoteId={quote.sentenceId} />
-              // )
-              if (quote.sentence.length > 50) {
-                quote.sentence = quote.sentence.slice(0, 50) + '...'
-              }
-
               return (
-                <div className='bg-white rounded-lg max-w-3xl w-full h-22'>
-                  <div className='m-5 flex flex-initial items-center max-w-5xl'>
-                    <div className='m-5 items-center'>
-                      <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold'>{quote.sentenceId}</h1>
-                    </div>
-                    <div>
-                      <h1 className='text-base md:text-lg lg:text-xl xl:text-2xl font-bold hover:underline items-center'>
-                        <Link href={`/quotes/${quote.sentenceId}`}><a>{quote.sentence}</a></Link></h1>
-                      <h3 className=''>โดย {quote.name}</h3>
-                    </div>
-                  </div>
-                </div>
+                <LongQuoteInfoCard
+                  key={quote.sentenceId} quote={quote.sentence}
+                  name={quote.name} quoteId={quote.sentenceId} />
               )
             })}
-          </InfiniteScroll>
-        </div>
-        <Footer />
-      </div>
+
+          </div>
+          <Footer />
+      </InfiniteScroll>
     )
   } catch (err) {
     console.log(err)
