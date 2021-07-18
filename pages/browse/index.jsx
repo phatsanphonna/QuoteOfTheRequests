2f8826f792ba95486fa71092a2d5c6f8b99fbbef
ssuniie <phatsanphon2004@outlook.com>
2021-07-18 21:53:12 +0700

    edit browse.jsx

--- pages/browse/index.jsx
+++ pages/browse/index.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 import { getAllJokes, getLimitJokes } from '@utils/databaseQuery/findData'
 
-import LongQuoteInfoCard from '@components/LoungQuoteInfoCard'
+import LongQuoteInfoCard from '@components/LongQuoteInfoCard'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -41,19 +42,45 @@ export default function index({ data, getTotalQuotes }) {
     return (
       <div className={mainTailwindCSS}>
         <Header />
-        <main className={mainTagTailwindCSS}>
+
+        <div className='flex flex-col items-center justify-center w-full flex-1 px-0'>
           <InfiniteScroll
             dataLength={quotes.length} next={getMoreQuotes} hasMore={hasMore}
-            loader={<h4>Loading...</h4>} endMessage={<b>End</b>} className='flex-col'
+            loader={
+              <h4 className='text-white text-lg m-10 flex flex-initial items-center justify-center drop-shadow-lg'>
+                โหลดเพิ่มเติมอยู่นร้าาา รอแป๊ปนึง</h4>}
+            endMessage={
+              <h3 className='text-white text-lg m-10 flex flex-initial items-center justify-center drop-shadow-lg'>
+                หมดแย้ว ๆ</h3>}
+            className='flex-col'
           >
             {quotes.map((quote) => {
+              // return (
+              //   <LongQuoteInfoCard
+              //     key={quote.sentenceId} quote={quote.sentence}
+              //     name={quote.name} quoteId={quote.sentenceId} />
+              // )
+              if (quote.sentence.length > 50) {
+                quote.sentence = quote.sentence.slice(0, 50) + '...'
+              }
+
               return (
-                  <LongQuoteInfoCard
-                    key={quote.sentenceId} quote={quote.sentence} name={quote.name} quoteId={quote.sentenceId} />
+                <div className='bg-white rounded-lg max-w-3xl w-full h-22'>
+                  <div className='m-5 flex flex-initial items-center max-w-5xl'>
+                    <div className='m-5 items-center'>
+                      <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold'>{quote.sentenceId}</h1>
+                    </div>
+                    <div>
+                      <h1 className='text-base md:text-lg lg:text-xl xl:text-2xl font-bold hover:underline items-center'>
+                        <Link href={`/quotes/${quote.sentenceId}`}><a>{quote.sentence}</a></Link></h1>
+                      <h3 className=''>โดย {quote.name}</h3>
+                    </div>
+                  </div>
+                </div>
               )
             })}
           </InfiniteScroll>
-        </main>
+        </div>
         <Footer />
       </div>
     )
